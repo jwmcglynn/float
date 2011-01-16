9d41fcacb4f8ebd6a1727d19a7ac4e6662604cba
rghassem <rghassem@gmail.com>
2011-01-16 13:26:58 +0000

    - Implemented HUD - Pause menu scales with screen now - Made pause menu look a little better - Added a bunch of new content under buttons.

--- Code/Menus/GamePauseMenu.cs
+++ Code/Menus/GamePauseMenu.cs
@@ -15,9 +15,9 @@ namespace Sputnik.Menus
             : base(menu)
         {
             Texture = menu.contentManager.Load<Texture2D>(textureName);
-            OnMouseOver += () => { VertexColor = Color.Aquamarine; };
+            OnMouseOver += () => { VertexColor = Color.LightGreen; };
             OnMouseOut += () => { VertexColor = Color.White; };
-            OnMouseDown += () => { VertexColor = Color.Blue; };
+            OnMouseDown += () => { VertexColor = Color.Green; };
         }
     }
 
@@ -27,42 +27,38 @@ namespace Sputnik.Menus
             :base(cntl, game)
         {
 
-            float halfButtonTextureHeight;
-            float halfButtonTextureWidth;
-
             MenuButton letterBackground = new MenuButton(this, "buttons\\pause_menu");
-            halfButtonTextureHeight = letterBackground.Texture.Height / 2;
-            halfButtonTextureWidth = letterBackground.Texture.Width / 2;
             letterBackground.Registration = letterBackground.Size / 2;
             letterBackground.PositionPercent = new Vector2(0.5f, 0.45f);
             letterBackground.Zindex = 0.6f;
             AddChild(letterBackground);
 
-            //TODO make the buttons.
+            //The buttons:
             MenuButton resumeGameButton = new MenuButton(this, "buttons\\back_to_game");
             MenuButton checkpointButton = new MenuButton(this, "buttons\\back_to_checkpoint");
             MenuButton startButton = new MenuButton(this, "buttons\\back_to_start");
             MenuButton titleButton = new MenuButton(this, "buttons\\back_to_title");
 
-            halfButtonTextureHeight = resumeGameButton.Texture.Height/2;
-            halfButtonTextureWidth = resumeGameButton.Texture.Width/2;
-			resumeGameButton.PositionPercent = new Vector2(0.5f,0.28f);
+            float distanceBetweenButtons = 65.0f;
+            float prevButton = -100.0f;
+
+            resumeGameButton.PositionPercent = new Vector2(0.5f, 0.45f);
             resumeGameButton.Registration = resumeGameButton.Size / 2;
-            resumeGameButton.CreateButton(new Rectangle(0, 0, 
-                (int)halfButtonTextureWidth * 2, (int)halfButtonTextureHeight * 2));
+            resumeGameButton.Position = (new Vector2(0.0f, prevButton)); 
+            resumeGameButton.CreateButton(new Rectangle(0, 0,
+                 (int)resumeGameButton.Size.X / 2, (int)resumeGameButton.Size.Y / 2));
             resumeGameButton.Zindex = 0.4f;
             resumeGameButton.OnActivate += () => {
                 onPressResume();
             };
 			AddChild(resumeGameButton);
 
-
-            halfButtonTextureHeight = checkpointButton.Texture.Height / 2;
-            halfButtonTextureWidth = checkpointButton.Texture.Width / 2;
-            checkpointButton.PositionPercent = new Vector2(0.5f, 0.58f);
+            checkpointButton.PositionPercent = new Vector2(0.5f, 0.45f);
             checkpointButton.Registration = checkpointButton.Size / 2;
+            prevButton += distanceBetweenButtons;
+            checkpointButton.Position = (new Vector2(15.0f, prevButton)); 
             checkpointButton.CreateButton(new Rectangle(0, 0,
-                (int)halfButtonTextureWidth * 2, (int)halfButtonTextureHeight * 2));
+                 (int)checkpointButton.Size.X / 2, (int)checkpointButton.Size.Y / 2));
             checkpointButton.Zindex = 0.5f;
             checkpointButton.OnActivate += () =>
             {
@@ -70,12 +66,12 @@ namespace Sputnik.Menus
             };
             AddChild(checkpointButton);
 
-            halfButtonTextureHeight = startButton.Texture.Height / 2;
-            halfButtonTextureWidth = startButton.Texture.Width / 2;
-            startButton.PositionPercent = new Vector2(0.5f, 0.38f);
+            startButton.PositionPercent = new Vector2(0.5f, 0.45f);
             startButton.Registration = startButton.Size / 2;
+            prevButton += distanceBetweenButtons;
+            startButton.Position = (new Vector2(0.0f, prevButton)); 
             startButton.CreateButton(new Rectangle(0, 0,
-                 (int)halfButtonTextureWidth * 2, (int)halfButtonTextureHeight * 2));
+                 (int)startButton.Size.X / 2, (int)startButton.Size.Y / 2));
             startButton.Zindex = 0.5f;
             startButton.OnActivate += () =>
             {
@@ -83,12 +79,12 @@ namespace Sputnik.Menus
             };
             AddChild(startButton);
 
-            halfButtonTextureHeight = titleButton.Texture.Height / 2;
-            halfButtonTextureWidth = titleButton.Texture.Width / 2;
-            titleButton.PositionPercent = new Vector2(0.5f, 0.48f);
+            titleButton.PositionPercent = new Vector2(0.5f, 0.45f);
             titleButton.Registration = titleButton.Size / 2;
+            prevButton += distanceBetweenButtons;
+            titleButton.Position = (new Vector2(0.0f, prevButton)); 
             titleButton.CreateButton(new Rectangle(0, 0,
-                 (int)halfButtonTextureWidth * 2, (int)halfButtonTextureHeight * 2));
+                 (int)titleButton.Size.X / 2, (int)titleButton.Size.Y / 2));
             titleButton.Zindex = 0.5f;
             titleButton.OnActivate += () =>
             {
--- Code/Menus/HUD.cs
+++ Code/Menus/HUD.cs
@@ -5,176 +5,99 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Sputnik.Game;
 
 namespace Sputnik.Menus {
-	class RectangleWidget : Widget {
-		public float Height;
-		public float FullWidth;
+	class HUDButton : Widget {
 
-		public float FillPercent = 1.0f;
-
-		public RectangleWidget(Menu env, float width, float height)
+        
+        private Color fillColor = Color.LightGray;
+        private Color highlightColor = Color.White;
+		public HUDButton(Menu env, string asset)
 			: base(env) {
-
-			Height = height;
-			FullWidth = width;
-
-			Texture2D dummyTexture = new Texture2D(env.Controller.GraphicsDevice, 1, 1);
-			dummyTexture.SetData(new Color[] { Color.White });
-
-			Texture = dummyTexture;
+     
+            Texture = env.contentManager.Load<Texture2D>(asset);
+			Registration = Size / 2;
+            PositionPercent = new Vector2(0.9f, 0.85f);
+            Zindex = 0.5f;
+            Scale = 0.65f;
+            VertexColor = fillColor;
+            Alpha = 0.7f;
+            
+            /* Are we going to implement controls here? -Reza
+            OnMouseOver += () => { Alpha = 1.0f; };
+            OnMouseOut += () => { deHighlight(); };
+            OnMouseDown += () => { highlight(); };
+             * */
 		}
 
-		public override void Draw(SpriteBatch spriteBatch) {
-			spriteBatch.Draw(Texture, AbsolutePosition, new Rectangle(
-				(int) Math.Round(-FullWidth / 2)
-				, (int) Math.Round(-Height / 2)
-				, (int) Math.Round(FullWidth * FillPercent)
-				, (int) Math.Round(Height)
-			), VertexColor * Alpha, 0.0f, new Vector2(FullWidth / 2, Height / 2), 1.0f, SpriteEffects.None, Zindex);
-		}
+        public void highlight()
+        {
+            VertexColor = highlightColor;
+            Alpha = 1.0f;
+        }
+        public void deHighlight()
+        {
+            VertexColor = fillColor;
+            Alpha = 0.7f;
+        }
 	}
 
 	public class HUD : Menu {
 		private GameEnvironment Environment;
 
-		private RectangleWidget up;
-		private RectangleWidget down;
-        private RectangleWidget left;
-        private RectangleWidget right;
-
-        private RectangleWidget hudB;
-
-        private RectangleWidget upPress;
-        private RectangleWidget downPress;
-        private RectangleWidget leftPress;
-        private RectangleWidget rightPress;
+		private HUDButton up;
+		private HUDButton down;
+        private HUDButton left;
+        private HUDButton right;
+        private HUDButton hudB;
 
 		public HUD(GameEnvironment env)
 				: base(env.Controller) {
 
 			Environment = env;
 
-			/////
-
-            hudB = new RectangleWidget(this, 200.0f, 200.0f);
-            hudB.PositionPercent = new Vector2(0.8f, 0.8f);
-            hudB.Position = new Vector2(0.8f, 0.8f);
-            hudB.VertexColor = Color.White;
+            hudB = new HUDButton(this, "buttons\\HUDOutline");
             hudB.Zindex = 0.7f;
-            hudB.FullWidth = 200.0f;
-            hudB.FillPercent = 1.0f;
             AddChild(hudB);
 
-			// Control Placeholder - Pomi edited this
-			up = new RectangleWidget(this, 0.1f, 10.0f);
-            //new RectangleWidget(this, ScreenSize.X * 0.25f - 4.0f, 20.0f - 4.0f);
-            up.PositionPercent = new Vector2(0.8f, 0.75f);
-            up.Position = new Vector2(0.8f, 0.8f);
-			up.VertexColor = Color.Yellow;
-			up.Zindex = 0.5f;
-            up.FullWidth = 20.0f;
-            up.FillPercent = 1.0f;
+			up = new HUDButton(this, "buttons\\TopButton");
 			AddChild(up);
 
-            down = new RectangleWidget(this, 0.1f, 10.0f);
-            down.PositionPercent = new Vector2(0.8f, 0.85f);
-            down.Position = new Vector2(0.8f, 0.8f);
-            down.VertexColor = Color.Pink;
-            down.Zindex = 0.5f;
-            down.FullWidth = 20.0f;
-            down.FillPercent = 1.0f;
+            down = new HUDButton(this, "buttons\\BottomButton");
             AddChild(down);
 
-            left = new RectangleWidget(this, 0.1f, 10.0f);
-            left.PositionPercent = new Vector2(0.75f, 0.8f);
-            left.Position = new Vector2(0.8f, 0.8f);
-            left.VertexColor = Color.Red;
-            left.Zindex = 0.5f;
-            left.FullWidth = 20.0f;
-            left.FillPercent = 1.0f;
+            left = new HUDButton(this, "buttons\\LeftButton");
             AddChild(left);
 
-            right = new RectangleWidget(this, 0.1f, 10.0f);
-            right.PositionPercent = new Vector2(0.85f, 0.8f);
-            right.Position = new Vector2(0.8f, 0.8f);
-            right.VertexColor = Color.Blue;
-            right.Zindex = 0.5f;
-            right.FullWidth = 20.0f;
-            right.FillPercent = 1.0f;
+            right = new HUDButton(this, "buttons\\RightButton");
             AddChild(right);
 
-            upPress = new RectangleWidget(this, 2.0f, 20.0f);
-            upPress.PositionPercent = new Vector2(0.8f, 0.75f);
-            upPress.Position = new Vector2(0.8f, 0.8f);
-            upPress.VertexColor = Color.Black;
-            upPress.Zindex = 0.6f;
-            upPress.FullWidth = 30.0f;
-            upPress.FillPercent = 0.0f;
-            AddChild(upPress);
-
-            downPress = new RectangleWidget(this, 2.0f, 20.0f);
-            downPress.PositionPercent = new Vector2(0.8f, 0.85f);
-            downPress.Position = new Vector2(0.8f, 0.8f);
-            downPress.VertexColor = Color.Black;
-            downPress.Zindex = 0.6f;
-            downPress.FullWidth = 30.0f;
-            downPress.FillPercent = 0.0f;
-            AddChild(downPress);
-
-            leftPress = new RectangleWidget(this, 2.0f, 20.0f);
-            leftPress.PositionPercent = new Vector2(0.75f, 0.8f);
-            leftPress.Position = new Vector2(0.8f, 0.8f);
-            leftPress.VertexColor = Color.Black;
-            leftPress.Zindex = 0.6f;
-            leftPress.FullWidth = 30.0f;
-            leftPress.FillPercent = 0.0f;
-            AddChild(leftPress);
-
-            rightPress = new RectangleWidget(this, 2.0f, 20.0f);
-            rightPress.PositionPercent = new Vector2(0.85f, 0.8f);
-            rightPress.Position = new Vector2(0.8f, 0.8f);
-            rightPress.VertexColor = Color.Black;
-            rightPress.Zindex = 0.6f;
-            rightPress.FullWidth = 30.0f;
-            rightPress.FillPercent = 0.0f;
-            AddChild(rightPress);
-
-
-
-            /*
-			upBG = new RectangleWidget(this, ScreenSize.X * 0.25f, 20.0f);
-			upBG.PositionPercent = new Vector2(0.0f, 1.0f);
-			upBG.Position = new Vector2(10.0f + ScreenSize.X * 0.25f / 2, -20.0f);
-			upBG.VertexColor = Color.Black;
-			upBG.Zindex = 1.0f;
-			AddChild(upBG);
-             */
 		}
 
 		public override void Update(float elapsedTime) {
 
             KeyboardState keyState = Keyboard.GetState();
             if (keyState.IsKeyDown(Keys.Up))
-                upPress.FillPercent = 1.0f;
+                up.highlight();
             else
-                upPress.FillPercent = 0.0f;
+                up.deHighlight();
+
             if (keyState.IsKeyDown(Keys.Down))
-                downPress.FillPercent = 1.0f;
+                down.highlight();
             else
-                downPress.FillPercent = 0.0f;
+                down.deHighlight();
+
             if (keyState.IsKeyDown(Keys.Left))
-                leftPress.FillPercent = 1.0f;
+                left.highlight();
             else
-                leftPress.FillPercent = 0.0f;
+                left.deHighlight();
+
             if (keyState.IsKeyDown(Keys.Right))
-                rightPress.FillPercent = 1.0f;
+                right.highlight();
             else
-                rightPress.FillPercent = 0.0f;
-
-
-            //upBG.FullWidth = ScreenSize.X * 0.25f;
-            //upBG.Position = new Vector2(10.0f + ScreenSize.X * 0.25f / 2, -20.0f);
+                right.deHighlight();
+            
 			base.Update(elapsedTime);
 		}
 	}
