95b2f5b677b273879954695d306bfd3db4bff237
rghassem <rghassem@gmail.com>
2011-01-17 05:07:08 +0000

    Added missing classes from last commit

---
+++ Code/Game/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Sputnik.Game
+{
+    class Checkpoint : Trigger
+    {
+        public Checkpoint(GameEnvironment env, SpawnPoint sp)
+            : base(env, sp)
+        {
+        }
+
+        public override void OnCollide(Entity entB, FarseerPhysics.Dynamics.Contacts.Contact contact)
+        {
+            base.OnCollide(entB, contact);
+             FarseerPhysics.Collision.WorldManifold manifold;
+			 contact.GetWorldManifold(out manifold);
+			 Vector2 posOfCollision = manifold.Points[0]*GameEnvironment.k_invPhysicsScale;
+             ((Balloon)entB).SpawnPoint.Position = entB.Position;
+        }
+    }
+}
---
+++ Code/Game/Trigger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics.Common;
+using FarseerPhysics.Dynamics;
+using Sputnik.Menus;
+
+namespace Sputnik.Game
+{
+    /// <summary>
+    /// Triggers collide only with the Balloon, and they're collision box is determined
+    /// through the editer
+    /// </summary>
+    class Trigger : GameEntity
+    {
+        private Rectangle shape;
+        public Trigger(GameEnvironment env, SpawnPoint sp)
+				: base(env, sp) {
+            shape = sp.Rect;
+			Initialize();
+			Position = sp.Position;
+		}
+
+		private void Initialize() {
+
+			CreateCollisionBody(Environment.CollisionWorld, BodyType.Kinematic, CollisionFlags.FixedRotation);
+
+            AddCollisionRectangle(new Vector2(shape.Width/2, shape.Height/2), Position);
+		}
+
+        public override bool ShouldCollide(Entity entB, Fixture fixture, Fixture entBFixture)
+        {
+            return (entB is Balloon);
+        }
+    }
+}
